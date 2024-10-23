71b14e73c32115409a254e77057dea08087e0a19
Andrew Haller <stairwaytowonderland@gmail.com>
2024-10-23 17:19:42 -0500

    Update profile.

--- etc/profile.d/02-umask.sh
+++ etc/profile.d/02-umask.sh
@@ -3,13 +3,16 @@
 
 _umask_hook() {
   if [ -n "$UMASK" -a "$UMASK" != "$DEFAULT_UMASK" ]; then
-    umask "$UMASK"
+    for d in $UMASK_OVERRIDE_DIRS; do
+      if [ "$PWD" = "$d" ]; then
+        umask "$UMASK"
+        printf "\033[0;2m%s\033[0m: \033[91;2m%s\033[0m=>\033[92;2m%s\033[0m\n" "Overriding default umask" "$DEFAULT_UMASK" "$UMASK"
+      fi
+    done
   else
     umask "$DEFAULT_UMASK"
   fi
 }
-
-printf "\033[0;2m%s\033[0m: \033[91;2m%s\033[0m=>\033[92;2m%s\033[0m\n" "Overriding default umask" "$DEFAULT_UMASK" "$UMASK"
 
 # Append `;` if PROMPT_COMMAND is not empty
 PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND;}_umask_hook"
