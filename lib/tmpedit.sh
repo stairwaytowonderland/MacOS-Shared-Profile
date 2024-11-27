03177836582452ff332f3ebdaa163f87fa012abf
Andrew Haller <andrewhaller101@gmail.com>
2024-11-27 16:41:36 -0600

    Update scripts.

--- lib/tmpedit.sh
+++ lib/tmpedit.sh
@@ -1,35 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-# Abstract:
-#     Script for using BBEdit as the default EDITOR.
-#
-# Details:
-#     This script allows BBEdit to be the `EDITOR` in a more robust way than
-#     the `bbedit` command line tool allows. This is because the `bbedit`
-#     command exits immediately after the file arguments are opened, unless the
-#     `--wait` switch is used. However, some tools, such as crontab, fail with
-#     a multiple-term `EDITOR`. This script can be set as the `EDITOR`,
-#     resolving that issue. The `--resume` switch is also used, which returns
-#     focus to the referring app (e.g., Terminal.app) after editing is complete.
-#
-# Usage:
-#     Save this script to somewhere like your ~/bin directory. You can then put
-#     something such as the following in your ~/.bashrc file::
-#
-#         # Set bbeditor as EDITOR.
-#         if [ -f "$HOME/bin/bbeditor" ]; then
-#             export EDITOR="$HOME/bin/bbeditor"
-#         else
-#             echo "WARNING: Can't find bbeditor."
-#         fi
-#
+set -eu
 
-if test -f "$(command -v bbedit)" ; then
+if test -x "$(command -v bbedit)" ; then
   # check stdin
-  if ! test -t 0 ; then
-    bbedit -t "Temporary Document -- Please Save" --language "Unix Shell Script" --new-window --clean
+  if test -t 0 ; then
+    set -x
+    bbedit -t "Temporary Document -- Please Save" --language "Unix Shell Script" --new-window --clean <<EOF
+$@
+EOF
   else
-    bbedit -t "Temporary Document -- Please Save" --language "Unix Shell Script" --new-window --clean <<<"$@"
+    set -x
+    bbedit -t "Temporary Document -- Please Save" --language "Unix Shell Script" --new-window --clean
   fi
 else
   echo "ERROR: Can't find bbedit. Did you install the command line tools?"
